6722df2a0fb8bd8da1901d89990c479a1b315cb4
Diogo Moitinho de Almeida <diogo149@gmail.com>
2014-10-27 20:30:35 -0700

    update project.clj metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject outfn "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A macro for clearer, more declarative functions."
+  :url "https://github.com/diogo149/outfn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[aysylu/loom "0.5.0"]
